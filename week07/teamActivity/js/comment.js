af8cb28e6b93a7861db855b12406632d0a974092
polsongarrett <garrettpolson94@gmail.com>
2021-06-03 15:20:24 -0600

    Added Jacobs files

---
+++ week07/teamActivity/js/comment.js
@@ -0,0 +1,9 @@
+
+export class Comment {
+  constructor(name, date, content, hikeId) {
+      this.name = name;
+      this.date = date;
+      this.content = content;
+      this.hikeId = hikeId;
+  }
+}
